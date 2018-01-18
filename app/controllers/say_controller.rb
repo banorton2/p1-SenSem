8067e4a3ba8cc37304bb2cf2c14d830689a311bd
Bethany Norton <annielulu93@yahoo.com>
2018-01-18 16:26:40 +0000

    Playtime: added filenames

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -1,8 +1,14 @@
 class SayController < ApplicationController
   def hello
     @time = Time.now
+    
   end
 
   def goodbye
+  end
+  
+  def filenames
+    @files = Dir.glob("*").sort
+    return @files
   end
 end
