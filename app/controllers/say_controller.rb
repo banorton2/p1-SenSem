95bd66efde1a6320234d707756cfeb18cac2b10f
Bethany Norton <annielulu93@yahoo.com>
2018-01-18 17:17:45 +0000

    Used Bootstrap.

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -1,14 +1,21 @@
 class SayController < ApplicationController
   def hello
-    @time = Time.now
-    
+    time_now
   end
 
   def goodbye
+    time_now
   end
   
   def filenames
+    time_now
     @files = Dir.glob("*").sort
     return @files
+  end
+  
+  private
+  
+  def time_now
+    @time = Time.now
   end
 end
